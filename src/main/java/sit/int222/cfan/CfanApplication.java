ec7c187ec234d7125111cc80e704a1db395334d0
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-08-24 17:13:12 +0700

    create project

---
+++ src/main/java/sit/int222/cfan/CfanApplication.java
@@ -0,0 +1,13 @@
+package sit.int222.cfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sit/int222/cfan/CfanApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int222.cfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
